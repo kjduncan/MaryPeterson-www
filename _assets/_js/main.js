82019b2c56e397cac7ff7afd4b1483332150fc57
sean <seaneduncan@gmail.com>
2017-01-24 21:08:21 -0700

    fixed various bugs

--- _assets/_js/main.js
+++ _assets/_js/main.js
@@ -1,4 +1,18 @@
 $(function() {
+    // Smooth scroll
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+            console.log(target.offset().top);
+          $('html, body').animate({
+            scrollTop: target.offset().top-100
+        }, 500);
+          return false;
+        }
+      }
+    });
 
   // Header nav-bar
   var bottomOfNav = $(".nav-bar").outerHeight(true) + $(".nav-bar").position().top;
